d691232e85af1550190b818f0180f81558193240
GuiUNG <46284418+GuiUNG@users.noreply.github.com>
2018-12-31 16:21:25 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
+const http = require('http');
 const fs = require('fs');
 
 http = require 'http'
