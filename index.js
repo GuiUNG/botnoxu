aa307bf4553112358bd9f37282bba5471b5197e6
GuiUNG <46284418+GuiUNG@users.noreply.github.com>
2018-12-31 18:42:22 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+require('http').createServer().listen(3000);
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 const fs = require('fs');
-require('http').createServer().listen(3000);
 
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
