121d42aa5d2965516527bdc466e8adb46f2f1979
GuiUNG <46284418+GuiUNG@users.noreply.github.com>
2018-12-31 18:44:47 -0200

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ client.on("message", message => {
     console.error(err)
                 return console.log('Não encontrei este comando');
   }
+fastify.listen(process.env.PORT || 3000, function (err) {
+  if (err) throw err
+  console.log(`server listening on ${fastify.server.address().port}`)
+}
  
 });
  
